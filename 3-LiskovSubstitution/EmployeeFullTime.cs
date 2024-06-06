cee338c1c25dddc13fc45df3fede0c7d04b9ed68
mbertolini29 <mbertolini29@users.noreply.github.com>
2024-06-06 17:56:15 -0300

    aprendimos el principio de sustitucion deliskv

--- 3-LiskovSubstitution/EmployeeFullTime.cs
+++ 3-LiskovSubstitution/EmployeeFullTime.cs
@@ -1,9 +1,20 @@
 namespace Liskov
 {
     public class EmployeeFullTime : Employee
-    {
-        public EmployeeFullTime(string fullname, int hoursWorked, int extrahours) : base(fullname, hoursWorked, extrahours)
+    {        
+        public int ExtraHours {get;set;}
+
+        public EmployeeFullTime(string fullname, int hoursWorked, int extraHours) : base(fullname, hoursWorked)
         {
+            //hay que iniciar propiedad en el constructor, 
+            //sino no suma las hs extras..
+            ExtraHours = extraHours; 
         }
+
+        public override decimal CalculateSalary ()
+        {   
+            decimal hourValue = 50;
+            return hourValue * (HoursWorked + ExtraHours);
+        } 
     }
 }
