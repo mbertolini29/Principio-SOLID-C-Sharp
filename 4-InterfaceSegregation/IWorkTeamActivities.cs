b570b75ba990fc39e9343a8eab052cf2324bfc1c
mbertolini29 <mbertolini29@users.noreply.github.com>
2024-06-06 21:26:39 -0300

    aprendimos el principio de segregacon de la interfaz

---
+++ 4-InterfaceSegregation/IWorkTeamActivities.cs
@@ -0,0 +1,10 @@
+namespace InterfaceSegregation
+{
+    public interface IWorkTeamActivities
+    {
+        //actividades, que normalmente,
+        //las hacen todo en equipo..
+        void Plan();
+        void Comunicate();        
+    }
+}
--- 4-InterfaceSegregation/Tester.cs
+++ 4-InterfaceSegregation/Tester.cs
@@ -1,29 +1,19 @@
 namespace InterfaceSegregation
 {
-    public class Tester : IActivities
+    public class Tester : IWorkTeamActivities, ITestActivities
     {
         public Tester()
         {
         }
 
-        public void Plan() 
+        public void Plan()
         {
-            throw new ArgumentException();
+            throw new NotImplementedException();
         }
-
-        public void Comunicate() 
+        
+        public void Comunicate()
         {
-            throw new ArgumentException();
-        }
-
-        public void Design() 
-        {
-            throw new ArgumentException();
-        }
-
-        public void Develop() 
-        {
-            Console.WriteLine("I'm developing the functionalities required");
+            throw new NotImplementedException();
         }
 
         public void Test() 
